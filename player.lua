93e5b9f9002e7019cc6323a0eb25b072e8e54b70
liquidberny <bernardo.urquijo.bu@gmail.com>
2025-08-12 20:16:42 -0700

    ahora ve atras

--- player.lua
+++ player.lua
@@ -10,46 +10,51 @@ function Player:new(x, y)
     self.idleImage = love.graphics.newImage("Sprites/Idle_1.png")
 
     local g = anim8.newGrid(16, 32, self.walkingImage:getWidth(), self.walkingImage:getHeight())
-    self.walkingAnimation = anim8.newAnimation(g('1-4', 1), 0.3)
-    self.idleAnimation = anim8.newAnimation(g('1-4', 1), 0.1)
+    self.animation = anim8.newAnimation(g('1-4', 1), 0.3)
 
     self.moving = false
+    self.forward = true
 end
 
 function Player:update(dt)
-
     self.moving = false
-
-    self.idleAnimation:update(dt)
+    self.animation:update(dt)
 
     if love.keyboard.isDown("left") then
         self.x = self.x - self.speed * dt
         self.moving = true
-        self.walkingAnimation:update(dt)
+
+        if self.forward then
+            self.forward = false
+            self.animation:flipH()
+        end
     end
+
     if love.keyboard.isDown("right") then
         self.x = self.x + self.speed * dt
-        self.walkingAnimation:update(dt)
         self.moving = true
+
+        if not self.forward then
+            self.forward = true
+            self.animation:flipH()
+        end
     end
+
     if love.keyboard.isDown("up") then
         self.y = self.y - self.speed * dt
-        self.walkingAnimation:update(dt)
         self.moving = true
     end
     if love.keyboard.isDown("down") then
         self.y = self.y + self.speed * dt
-        self.walkingAnimation:update(dt)
         self.moving = true
     end
-
 end
 
+
 function Player:draw()
-    local anim = self.moving and self.walkingAnimation or self.idleAnimation
     local img = self.moving and self.walkingImage or self.idleImage
 
-    anim:draw(img, self.x, self.y, 0 , 3, 3)
+    self.animation:draw(img, self.x, self.y, 0 , 3, 3)
 end
 
 return Player
