92037f5b74ce708f406bcb951db1b0ea03cb9a8f
liquidberny <bernardo.urquijo.bu@gmail.com>
2025-08-24 15:33:40 -0700

    primeras colisiones

--- main.lua
+++ main.lua
@@ -1,4 +1,34 @@
 local p1
+local bump = require("lib.bump")
+local world = bump.newWorld()
+
+-- helper function
+local function drawBox(box, r, g, b)
+    love.graphics.setColor(r, g, b, 0.25)
+    love.graphics.rectangle("fill", box.x, box.y, box.w, box.h)
+    love.graphics.setColor(r, g, b)
+    love.graphics.rectangle("line", box.x, box.y, box.w, box.h)
+    love.graphics.reset()
+end
+
+local blocks = {}
+
+local function addBlock(x, y, w, h)
+    local block = {
+        x = x,
+        y = y,
+        w = w,
+        h = h
+    }
+    blocks[#blocks + 1] = block
+    world:add(block, x, y, w, h)
+end
+
+local function drawBlocks()
+    for _, block in ipairs(blocks) do
+        drawBox(block, 1, 0, 0)
+    end
+end
 
 function love.load()
     Object = require("lib.classic")
@@ -8,13 +38,17 @@ function love.load()
     p1 = Player(100, 200)
     camera = require("lib.camera")
     cam = camera(p1.x, p1.y, 2)
+
+    world:add(p1, p1.x, p1.y, 16, 32)
+
+    addBlock(200, 250, 16, 32)
 end
 
 function love.update(dt)
     local dx, dy = p1.x - cam.x, p1.y - cam.y
     cam:move(dx / 2, dy / 2)
 
-    p1:update(dt)
+    p1:update(dt, world)
     local w = love.graphics.getWidth()
     local h = love.graphics.getHeight()
 
@@ -29,12 +63,12 @@ function love.update(dt)
         cam.x = mapW - w / 4
     end
 
-    if cam.y < h/4 then
-      cam.y = h / 4
+    if cam.y < h / 4 then
+        cam.y = h / 4
     end
 
-    if cam.y > mapH - h /4 then
-      cam.y = mapH - h /4
+    if cam.y > mapH - h / 4 then
+        cam.y = mapH - h / 4
     end
 
 end
@@ -43,5 +77,7 @@ function love.draw()
     cam:attach()
     gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
     p1:draw()
+    
+    drawBlocks()
     cam:detach()
 end
--- player.lua
+++ player.lua
@@ -11,29 +11,31 @@ function Player:new(x, y)
     self.punchImage = love.graphics.newImage("Sprites/Punch.png")
     local g = anim8.newGrid(16, 32, self.walkingImage:getWidth(), self.walkingImage:getHeight())
     local singleG = anim8.newGrid(20, 32, self.punchImage:getWidth(), self.punchImage:getHeight())
-    
+
     self.animation = anim8.newAnimation(g('1-4', 1), 0.3)
-    self.punch = anim8.newAnimation(singleG(1,1), 0.5 )
+    self.punch = anim8.newAnimation(singleG(1, 1), 0.5)
 
     self.moving = false
     self.punching = false
     self.forward = true
 end
 
-function Player:update(dt)
+function Player:update(dt, world)
     self.moving = false
     self.punching = false
 
-
     self.animation:update(dt)
-    
+
+    local speed = self.speed
+    local dx, dy = 0, 0
+
     if love.keyboard.isDown("z") then
         self.moving = false
         self.punching = true
     end
 
     if love.keyboard.isDown("left") and self.punching == false then
-        self.x = self.x - self.speed * dt
+        dx = -speed * dt
         self.moving = true
 
         if self.forward then
@@ -41,10 +43,8 @@ function Player:update(dt)
             self.animation:flipH()
             self.punch:flipH()
         end
-    end
-
-    if love.keyboard.isDown("right") and self.punching == false then
-        self.x = self.x + self.speed * dt
+    elseif love.keyboard.isDown("right") and self.punching == false then
+        dx = speed * dt
         self.moving = true
 
         if not self.forward then
@@ -55,19 +55,21 @@ function Player:update(dt)
     end
 
     if love.keyboard.isDown("up") and self.punching == false then
-        self.y = self.y - self.speed * dt
+        dy = -speed * dt
+        self.moving = true
+    elseif love.keyboard.isDown("down") and self.punching == false then
+        dy = speed * dt
         self.moving = true
     end
-    if love.keyboard.isDown("down") and self.punching == false then
-        self.y = self.y + self.speed * dt
-        self.moving = true
-    end
-end
 
+    if dx ~= 0 or dy ~= 0 then
+        self.x, self.y = world:move(self, self.x + dx, self.y + dy)
+    end
+
+end
 
 function Player:draw()
     local img = self.moving and self.walkingImage or self.punching and self.punchImage or self.idleImage
-
 
     local animation = self.punching and self.punch or self.animation
     animation:draw(img, self.x, self.y)
