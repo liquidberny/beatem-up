cb113676d832f4c01df3e15783da70451237c195
liquidberny <bernardo.urquijo.bu@gmail.com>
2026-08-05 17:14:35 -0700

    Agregar footprint y filtro de movimiento compartidos a Character

--- character.lua
+++ character.lua
@@ -1,11 +1,17 @@
 local Character = Object:extend()
 
+Character.footprintWidth = 16
+Character.footprintHeight = 10
+Character.footprintOffsetX = 0
+Character.footprintOffsetY = 22
+
 function Character:new(x, y, speed)
     self.x = x
     self.y = y
     self.health = 100
     self.maxHealth = 100
     self.speed = speed or 100
+    self.isCharacter = true
 end
 
 function Character:takeDamage(amount)
@@ -14,6 +20,13 @@ end
 
 function Character:isDead()
     return self.health <= 0
+end
+
+function Character.movementFilter(item, other)
+    if other.isCharacter then
+        return nil
+    end
+    return "slide"
 end
 
 return Character
